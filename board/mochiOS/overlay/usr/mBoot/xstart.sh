91c6e33b642414f418d55da0e7528db1119468af
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-05-04 17:06:02 +0900

    一応イメージが作成できてQemuが起動するところまで

--- board/mochiOS/overlay/usr/mBoot/xstart.sh
+++ board/mochiOS/overlay/usr/mBoot/xstart.sh
@@ -3,22 +3,25 @@
 echo "[mBoot] setting env..."
 OVMF="/usr/mBoot/OVMF.fd"
 TARGET_IMG="/usr/mBoot/mochiOS.img"
+ESP_IMG="/usr/mBoot/esp.img"
 echo "[mBoot] setted"
 
 echo "[mBoot] enabling kvm..."
 KVM_ARGS=""
 if [ -e /dev/kvm ]; then
-    KVM_ARGS="-enable-kvm -cpu host"
+    ACCEL_ARGS="-enable-kvm -cpu host"
     echo "[mBoot] enabled kvm"
 else
+    ACCEL_ARGS="-accel tcg,thread=single -cpu max"
     echo "[mBoot] Your processor does not have KVM support."
 fi
 
 echo "[mBoot] Booting mochiOS..."
 exec qemu-system-x86_64 \
-    $KVM_ARGS \
+    $ACCEL_ARGS \
     -bios "$OVMF" \
-    -drive format=raw,file="$TARGET_IMG",media=disk \
+    -drive format=raw,file="$ESP_IMG",index=0,media=disk \
+    -drive format=raw,file="$TARGET_IMG",index=1,media=disk \
     -usb \
     -device qemu-xhci,id=xhci \
     -device usb-kbd,bus=xhci.0 \
@@ -27,5 +30,5 @@ exec qemu-system-x86_64 \
     -m 512M \
     -serial stdio \
     -vga std \
-    -display sdl \
+    -display sdl,show-cursor=off
     -full-screen
